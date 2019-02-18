071f35815c4650a9bfbb28398139aae98c5fe69e
Alisson Rangel <alisson.rangel@hotmail.com>
2019-02-18 08:12:43 -0300

    criacao da revisao do projeto02

---
+++ index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="assets/css/estilo.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,user-scalable=0">
+        <script type="text/javascript" src="assets/js/script.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+              integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+              crossorigin="anonymous">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <div class="menu">
+                    <nav>
+                        <ul>
+                            <li><a href="" class="ativado">Home</a></li>
+                            <li><a href="">Quem somos</a></li>
+                            <li><a href="">Anuncie aqui</a></li>
+                            <li><a href="">Contato</a></li>
+                        </ul>
+
+                    </nav>
+                </div>
+                <div class="social">
+                    <input type="image" src="assets/images/lupa.png" class="image" border="0" width="26" height="26">
+                    <input type="text" class="busca" name="pesquisa" placeholder="Buscar...">
+                    <img src="assets/images/youtube.png">
+                    <img src="assets/images/googleplus.png">
+                    <img src="assets/images/facebook.png">
+                    <img src="assets/images/rss.png">
+                    <img src="assets/images/twitter.png">
+
+
+                </div>
+            </div>
+        </header>
+        <div class="banner">
+            <div class="container">
+                <div class="logo">
+                    <img src="assets/images/logo2.png" width="200">
+
+                    </img>
+                </div>
+                <div class="publicidade">
+                    PUBLICIDADE 728X90
+                </div>
+            </div>
+        </div>
+        <div class="menu_principal">
+            <div class="container">
+                <div class="menudd">
+                    <nav>
+                        <ul>
+                            <li class="ativado"><a href="" >Home</a></li>
+                            <li><a href="">Economia</a></li>
+                            <li >
+
+                                <a href="" >
+                                    Entretenimento&nbsp;&nbsp;
+                                    <i class="fas fa-angle-down"></i>
+                                </a>
+                                <div class="submenu">
+                                    <ul>
+                                        <li><a href="">Sub menu 1</a></li>
+                                        <li>
+                                            <a href="" >
+                                                Sub menu 2&nbsp;&nbsp;&nbsp;&nbsp;
+                                                <i class="fas fa-angle-right"></i>
+                                            </a>
+                                            <div class="subsubmenu">
+                                                <ul>
+                                                    <li><a href="">Sub sub 1</a></li>
+                                                    <li><a href="">Sub sub 2</a></li>
+                                                    <li><a href="">Sub sub 3</a></li>
+                                                </ul>
+                                            </div>
+                                        </li>
+                                        <li><a href="">Sub menu 3</a></li>
+                                        <li><a href="">Sub menu 4</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><a href="">Esportes</a></li>
+                            <li ><a href="" >Geral</a></li>
+                            <li>
+                                <a href="">
+                                    Notícias&nbsp;&nbsp;
+                                    <i class="fas fa-angle-down"></i>
+                                </a>
+                                <div class="submenu">
+                                    <ul>
+                                        <li><a href="">Sub menu 1</a></li>
+                                        <li><a href="">Sub menu 2</a></li>
+                                        <li><a href="">Sub menu 3</a></li>
+                                        <li><a href="">Sub menu 4</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li ><a href="" >Polícia</a></li>
+                            <li><a href="">Videos</a></li>
+                        </ul>
+
+                    </nav>
+
+                </div>
+            </div>
+
+        </div>
+        <div class="ultimas">
+            <div class="container">
+                <div class="vermelha">
+                    Últimas notícias
+                </div>
+                <div class="branca">
+                    Incendio no CT do Flamengo na noite de ontem mata 10 adolesentes
+                </div>
+            </div>
+        </div>
+
+        <div class="principal">
+            <div class="container">
+                <section class="esquerda">
+
+                </section>
+                <section class="direita">
+                    <aside class="widget">
+                        <div class="titulo">
+                            Social
+                        </div>
+                        <div class="conteudo">
+                            <img src="assets/images/youtube.png" width=18" height="18" >
+                            <img src="assets/images/googleplus.png" width=18" height="18">
+                            <img src="assets/images/facebook.png" width=18" height="18">
+                            <img src="assets/images/rss.png" width=18" height="18">
+                            <img src="assets/images/twitter.png" width=18" height="18">
+                        </div>
+                    </aside>
+                    <aside class="widget">
+                        <div class="titulo">
+                            Últimas notícias
+                        </div>
+                        <div class="conteudo">
+                            <a href=""> 
+                                <div class="um">
+                                    Policia militar prende quadrilha com drogas e 
+                                    metralhadoras na zona norte de Teresina
+                                </div>
+                            </a>
+                            <a href=""> 
+                                <div class="um">
+                                    Em retorno ao Mineirao Cruzeiro empata por
+                                    1 x 1 com a Caldense no Campeonato mineiro
+                                </div>
+                            </a>
+                            <a href=""> 
+                                <div class="um">
+                                    Policia militar prende quadrilha com drogas e 
+                                    metralhadoras na zona norte de Teresina
+                                </div>
+                            </a>
+                        </div>
+                    </aside>
+                    <aside class="widget">
+                        <div class="titulo">
+                            Publicidade
+                        </div>
+                        <div class="conteudo">
+                            <img src="assets/images/banner.jpg" width="270">
+                        </div>
+                    </aside>
+                    <aside class="widget">
+                        <div class="titulo">
+                            Encontre-nos no facebook
+                        </div>
+                        <div class="conteudo">
+                            <iframe src="https://www.facebook.com/plugins/page.php?
+                                    href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs&
+                                    width=267&height=214&small_header=false&adapt_container_wi
+                                    dth=true&hide_cover=false&show_facepile=true&appId" 
+                                    width="267" height="214" 
+                                    style="border:none;overflow:hidden" 
+                                    scrolling="no" frameborder="0" 
+                                    allowTransparency="true" allow="encrypted-media"></iframe>
+                        </div>
+                    </aside>
+                </section>
+            </div>
+        </div>
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
